b8c66664762f77c501ed7ed02747e78ac7d56f70
Mariannkr <mariano.carro73@gmail.com>
2022-09-21 17:05:53 -0300

    finish

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log('funciona?, si papá')
+
+const productName = document.querySelector('#productName');
+const productPrice = document.querySelector('#productPrice');
+const buttonSave = document.querySelector('#button-save');
+const buttonCancel = document.querySelector('#button-cancel');
+const productList = document.querySelector('#product-list');
+const totalOutput = document.querySelector('#total');
+
+let total = 0;
+
+const createNewProduct = (name, price) => {
+    const ionCard = document.createElement('ion-card');
+    const newProductItem = document.createElement('ion-card-content');
+    newProductItem.textContent = name + ': $' + price;
+    ionCard.appendChild(newProductItem);
+    productList.appendChild(ionCard); 
+
+}
+
+const clearInputs = () => {
+    productName.value = '';
+    productPrice.value = '';
+}
+
+const presentAlert = () => {
+    const alert = document.createElement('ion-alert')
+    alert.header = 'datos invalidos';
+    alert.subHeader = 'Por favor verifique los datos';
+    alert.message = 'nombre o precio incorrectos';
+    alert.buttons = ['Ok'];
+
+    document.body.appendChild(alert);
+    return alert.present();
+}
+
+const isEmpty = str => !str.trim().length;
+
+buttonSave.addEventListener('click', () => {
+    const name = productName.value;
+    const price = productPrice.value;
+
+    if (isEmpty(name) || price <= 0 || isEmpty(price)) {
+        presentAlert();
+        return;
+    }
+
+    createNewProduct(name, price);
+    total += +price;
+    totalOutput.textContent = total;
+    clearInputs();
+})
+
+buttonCancel.addEventListener('click', clearInputs);
